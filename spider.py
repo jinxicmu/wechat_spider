43a3b245da13be6265cc107f1b2b9ce42f7b39f9
jinxi@andrew.cmu.edu <jinxi@andrew.cmu.edu>
2016-12-18 11:43:18 -0800

    .

--- spider.py
+++ spider.py
@@ -8,6 +8,20 @@ url = 'http://mp.weixin.qq.com/s' \
       '&chksm=870fcb96b078428015ee46e5cf6ac294879ccd8eb72e6e7523a0b7d3f70ade373a01ef334761' \
       '&mpshare=1&scene=1&srcid=1218877mbxp8q9wEPP2SGJdX#rd'
 response = requests.get(url)
+
+file = open("content.html", "w+")
+file.write(response.content)
+
 parsed_html = BeautifulSoup(response.content)
-for imgsc in parsed_html.body.findAll('img', {'data-type': 'jpeg'}):
+girl_imgs = parsed_html.body.findAll('img', {'data-type': 'jpeg'})[: -2]
+for imgsc in girl_imgs:
     print imgsc['data-src']
+
+info = parsed_html.body.find('div', {'id': 'js_content'})
+girl_infos = info.text.split('dicksoup')
+for girl_info in girl_infos[1 :]:
+    print girl_info
+
+if (len(girl_imgs) != len(girl_infos)):
+    raise ValueError("Not equal size for girl_imgs and girl_infos, girl_imgs: "
+                     , len(girl_imgs), "girl_infos: ", len(girl_infos))
