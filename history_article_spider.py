e0f89e9a94d08c78eb6ee7c0618e4816a795d219
jinxi@andrew.cmu.edu <jinxi@andrew.cmu.edu>
2016-12-23 17:30:02 -0800

    Get wechat history articles

--- history_article_spider.py
+++ history_article_spider.py
@@ -1,17 +1,66 @@
-#coding:utf-8
-from selenium import webdriver
-import time
-driver = webdriver.Chrome()
-driver.get('http://weixin.sogou.com/')
-driver.find_element_by_id("upquery").send_keys(u"浙江旅游")
-driver.find_element_by_class_name("swz2").click()
-now_handle = driver.current_window_handle
-driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[1]/div/div[2]/div/div[1]').click()
-time.sleep(10)
-all_handle = driver.window_handles
-for handle in all_handle:
-    print handle
-if handle != now_handle:
-    driver.switch_to_window(handle)
-print u"当前网页title" + driver.title
-#driver.quit()
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" How to get cookie? Apparently wechat is using a SSL authentication mechanism to ensure that only the wechat inner browser
+can open the wechat subscriptions. I tried fiddler to decode the cookie used here, but failed since fiddler is giving me
+error that cannot decode the SSL.
+Then I upgrate Chrome to newest version 55.0.2883.95, open the wechat app on mac and open the subscriptions article.
+Then click the additional Chrome icon from wechat inner browser. Boom! All articles loaded! Then I found that Chrome decode
+the cookie used here and added them to the request header.
+Having the cookie, things become much easier. Only need to compare the difference between urls when load different article list,
+and we can change the frommsgid to set the start index of article. -- failed.
+Finally get the whole webpage through chrome.
+"""
+
+
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+html_file = open("history.html", "r")
+parsed_html = BeautifulSoup(html_file.read())
+articles1 = parsed_html.body.findAll('div', {'class': 'msg_item news redirect'})
+articles2 = parsed_html.body.findAll('div', {'class': 'msg_item multi_news'})
+
+article_links = open("articles", "w+")
+i = 1
+for article in articles1:
+    try:
+        article_title = article.find('h4', {'class': 'msg_title'}).text
+        article_title = article_title.encode('utf-8')
+        if article_title.find(u'女生场'.encode('utf-8')) != -1:
+            article_url = article.find('div', {'class': 'msg_cover'})['hrefs']
+            print  i, article_title, article_url
+            i = i + 1
+            article_links.write(article_url)
+            article_links.write("\n")
+    except KeyError:
+        pass
+
+for article in articles2:
+    try:
+        article_title = article.find('h4', {'class': 'msg_title'}).text.encode('utf-8')
+        article_url = article.find('h4', {'class': 'msg_title'})['hrefs']
+        if article_title.find(u'女生场'.encode('utf-8')) != -1:
+            print i, article_title, article_url
+            i = i + 1
+            article_links.write(article_url)
+            article_links.write("\n")
+    except KeyError:
+        pass
+
+
+def compare(s1, s2):
+    print len(s1), len(s2)
+    for i in range(len(s1)):
+        if (s1[i] != s2[i]):
+            print i, s1[i]
+
+def openUrlWithCookie(url):
+    opener = urllib2.build_opener()
+    opener.addheaders.append(('Cookie',
+                              'wap_sid=CP/D4IgKEkBrdlpJS1U3UFZQenlRNFdQM3J5UmhIUmo0QnZJNUZUaHBYNkxBbm9NdFJ4Unc0U2RBVHV4dFI0TkE3Q2N4anZ5GAQg/REo8L/4uQsw9NHywgU=; '
+                              'wap_sid2=CP/D4IgKElxpOTZDRDNVVC1SZ2JZUTd0cmYwOE5WWTRsaW5jNmV3TFdjdHNkUXQ5T3pWcDVBdnNwUEVucWpnWk43MjEzUXlrWGVnMC1BNzFyLVFXWmhvR3hlZEF1M1FEQUFBfg=='))
+    opener.addheaders.append(('User-Agent',
+                              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'))
+    f = opener.open(url)
+    return f.read()
--- spider.py
+++ spider.py
@@ -44,28 +44,36 @@ def save_girl_img_to_html(img_src, id, html_file):
     html_file.write("\n")
 
 def save_girls_to_html(img_srcs, infos):
-    html_file = open("content.html", "w+")
-    if (len(girl_imgs) != len(girl_infos)):
-        raise ValueError("Not equal size for girl_imgs and girl_infos, girl_imgs: "
-                         , len(girl_imgs), "girl_infos: ", len(girl_infos))
+    html_file = open("content.html", "a")
+    # if (len(img_srcs) != len(infos)):
+    #     raise ValueError("Not equal size for girl_imgs and girl_infos, girl_imgs: "
+    #                      , len(img_srcs), "girl_infos: ", len(infos))
     girl_ids = []
-    for girl_info in girl_infos:
+    for girl_info in infos:
         girl_ids.append(girl_info[7: 11])
 
-    for i in range(len(girl_imgs)):
-        save_girl_img_to_html(girl_imgs[i], girl_infos[i], html_file)
-
+    for i in range(min(len(img_srcs), len(infos))):
+        save_girl_img_to_html(img_srcs[i], infos[i], html_file)
 
 url = 'http://mp.weixin.qq.com/s' \
       '?__biz=MzA3NDMwMTkzNg==&mid=2650440457&idx=1&sn=1a260f8d71e3ab0d11d820ed56d7b842' \
       '&chksm=870fcb96b078428015ee46e5cf6ac294879ccd8eb72e6e7523a0b7d3f70ade373a01ef334761' \
       '&mpshare=1&scene=1&srcid=1218877mbxp8q9wEPP2SGJdX#rd'
-response = requests.get(url)
-parsed_html = BeautifulSoup(response.content)
-girl_imgs = get_girl_imgs(parsed_html)
-raw_girl_infos = get_girl_infos(parsed_html)
-girl_infos = formalize_girl_infos(raw_girl_infos)
-save_girls_to_html(girl_imgs, girl_infos)
 
-for formal_girl_info in girl_infos:
-    print formal_girl_info
+
+def get_article_content(url):
+    response = requests.get(url)
+    parsed_html = BeautifulSoup(response.content)
+    girl_imgs = get_girl_imgs(parsed_html)
+    raw_girl_infos = get_girl_infos(parsed_html)
+    girl_infos = formalize_girl_infos(raw_girl_infos)
+    # if (len(girl_imgs) == len(girl_infos)):
+    save_girls_to_html(girl_imgs, girl_infos)
+    for formal_girl_info in girl_infos:
+        print formal_girl_info
+
+
+articles = open('articles', 'r')
+for line in articles:
+    print line
+    get_article_content(line)
